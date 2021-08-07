b89be7d1c0de778e22996cef58ec6fabb1e657ee
Suplee06 <88596277+Suplee06@users.noreply.github.com>
2021-08-08 01:19:13 +0530

    Add files via upload

---
+++ LearnDatatypesAndVariables.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class LearnDatatypesAndVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Brand ="Apple";
+		String Model ="11 Pro";
+		int price =500000;
+		byte memory =122;
+		float screenDisplay = 5.5f;
+		boolean touchScreen = true;
+		long phoneNumber = 9952724424l;
+		
+		System.out.println(" phone brand is " + Brand);
+		System.out.println(" phone model is " + Model);
+		System.out.println(" phone price is " + price);
+		System.out.println(" phone memory is " +memory);
+		System.out.println(" phone screen Display size is " + screenDisplay);
+		System.out.println(" phone has touchscreen enabled " + touchScreen);
+		System.out.println(" phoneNumber is " + phoneNumber);
+
+
+	}
+
+}
---
+++ LoopAndConditional.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class LoopAndConditional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		
+		for(i=1;i<=50;i++) {
+			
+			if(i%2!=0) {
+				System.out.println(i + " is odd");
+			}
+			else 
+				System.out.println(i + " is even");
+		}
+			}
+		}
+	
+
+
