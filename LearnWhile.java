efcb988ae404d282e0b6dc4801947183fb4a8394
Suplee06 <88596277+Suplee06@users.noreply.github.com>
2021-08-08 12:45:41 +0530

    Add files via upload

--- LearnWhile.java
+++ LearnWhile.java
@@ -8,13 +8,11 @@ public class LearnWhile {
           System.out.println("enter the number to find the sum of digits :" +num );
           
           while(num!= 0) {
-        	 
-        	  
+        	   
         	  rem=num%10;
         	  sum=sum+rem;
         	  num=num/10;
-        
-              
+            
           }
           System.out.println("Sum of the number is :" +sum );
           
